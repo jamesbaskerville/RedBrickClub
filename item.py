d8cfe92c9c2fa9fc8b79ed211fc4738831bbbe02
James Baskerville <baskerville@college.harvard.edu>
2017-11-30 02:26:31 -0500

    add class method and start item

---
+++ item.py
@@ -0,0 +1,22 @@
+from user import User
+'''
+    Item:
+    An item in the exchange market.
+
+    Attributes:
+    token:          (URL-safe) unique identifier
+    user:           user that owns this item
+    description:    text description of this item
+'''
+class Item:
+    def __init__(self, user_token, description):
+        self.token = secrets.token_urlsafe()
+        self.user = User.find_by_user_token(user_token)
+        self.description = description
+
+    def add_or_replace_description(self, description):
+        self.description = description
+
+    def add_picture(self, picture):
+        # TODO ???
+        raise NotImplemented
