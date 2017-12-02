9c8c0fe4d60a9ffc7d4e565dc02a105a71cf3246
James Baskerville <baskerville@college.harvard.edu>
2017-12-02 16:50:16 -0500

    added some measurement tracking

--- sim/sim.py
+++ sim/sim.py
@@ -6,14 +6,20 @@ import ttc
 # anything else?
 ranks = []
 rounds = []
+
+ttc.configure_logging('warning')
 for prop in xrange(1, 11, 1):
     res = ttc.run(1000, prop/10.0, seed = 234)
-    ranks.append((prop/10.0, sum(res[1])))
+    ranks.append((prop/10.0, res[1]))
     rounds.append((prop/10.0, res[2]))
 
-print "\nResults:"
-for i in xrange(len(ranks)):
-    print "{}% reported preferences: {} ranks; {} rounds".format(ranks[i][0]*100,
-                                                                  ranks[i][1],
-                                                                  rounds[i][1])
+ttc_out = ranks[9][1]
 
+print "Results (pos/neg effects vs. TTC outcome)"
+print "-----------------------------------------"
+for prop, out in ranks:
+    neg, pos = ttc.compare_outcomes(ttc_out, out)
+    print "{}%: (+{}, -{}), net difference: {}".format(int(prop * 100),
+                                                       pos,
+                                                       neg,
+                                                       pos - neg)
--- sim/ttc.py
+++ sim/ttc.py
@@ -1,15 +1,17 @@
 from agent import Agent
 import random
+import logging
+import sys
 
 def run(n, report_prop, seed = None, debug = False):
-    print "\nBeginning Run of TTC Algorithm"
+    logging.info("\nBeginning Run of TTC Algorithm")
     random.seed(seed)
 
     agents = []
     k = int(report_prop * n)
     available_agents = set(range(n))
 
-    print "Assigning initial true and reported preferences..."
+    logging.info("Assigning initial true and reported preferences...")
     for i in xrange(n):
         new_agent = Agent(i)
         true_prefs, rep_prefs = gen_prefs(n, k, debug)
@@ -20,16 +22,16 @@ def run(n, report_prop, seed = None, debug = False):
 
     assigned_items = [-1] * n
 
-    print "Running TTC..."
+    logging.info("Running TTC...")
     rounds = 0
     while available_agents:
         rounds += 1
-        print "Round {}".format(rounds)
+        logging.info("Round {}".format(rounds))
         # update top preferences ("pointing"/directed edges in graph)
         for a_id in available_agents:
             agents[a_id].update_top_pref(available_agents)
-            if debug:
-                print agents[a_id], "prefs:", agents[a_id].get_top_pref()
+            logging.debug("{} ; top_pref: {}".
+                          format(agents[a_id], agents[a_id].get_top_pref()))
 
         # find a cycle
         visited = set()
@@ -41,30 +43,25 @@ def run(n, report_prop, seed = None, debug = False):
         stack.append(a_id)
 
         top_pref = agents[a_id].get_top_pref()
-        if debug:
-            print "starting DFS from agent {}".format(a_id)
-            print "stack:", stack
+        logging.debug("starting DFS from agent {}".format(a_id))
+        logging.debug("stack:{}".format(stack))
 
         while top_pref not in visited:
             visited.add(top_pref)
             stack.append(top_pref)
             top_pref = agents[top_pref].get_top_pref()
-            if debug:
-                print "stack:", stack
+            logging.debug("stack:{}".format(stack))
 
         # get cycle
         cycle = stack[stack.index(top_pref):] + [top_pref]
-        if debug:
-            print "cycle found:", cycle
+        logging.debug("cycle found: {}".format(cycle))
         # assign matchings
         for i in xrange(len(cycle) - 1):
             assigned_items[cycle[i]] = cycle[i + 1]
-        if debug:
-            print "updated matchings:", assigned_items
+        logging.debug("updated matchings: {}".format(assigned_items))
         # remove cycle from market
         available_agents -= set(cycle)
-        if debug:
-            print "remaining agents:", available_agents, "\n"
+        logging.debug("remaining agents: {}\n".format(available_agents))
     return assigned_items, get_pref_outcomes(agents, assigned_items), rounds
 
 # a list of the result of the algorithm for each agent
@@ -85,10 +82,28 @@ def gen_prefs(n, k, debug = False):
     rep_prefs = map(lambda (_, p): p,
                     filter(lambda (a, _): a in rep_indices,
                            enumerate(true_prefs)))
-    if debug:
-        print true_prefs, rep_indices, rep_prefs
+    logging.debug("{} {} {}".format(true_prefs, rep_indices, rep_prefs))
     return true_prefs, rep_prefs
 
+def configure_logging(loglevel):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    root_logger = logging.getLogger('')
+    strm_out = logging.StreamHandler(sys.__stdout__)
+    strm_out.setFormatter(logging.Formatter('%(message)s'))
+    root_logger.setLevel(numeric_level)
+    root_logger.addHandler(strm_out)
+
+# compare two pref outcomes, calculating the number of agents who do worse
+def compare_outcomes(ttc, other):
+    return (len(filter(lambda (t, o): t < o, zip(ttc, other))),
+            len(filter(lambda (t, o): t > o, zip(ttc, other))))
+
 if __name__ == '__main__':
-    res = run(10, 0.75, seed = 124, debug=True)
-    print res
+    configure_logging('info')
+    res = run(10, 0.75, seed = 124)
+    print "items:    {}".format(res[0])
+    print "outcomes: {}".format(res[1])
+    print "rounds:   {}".format(res[2])
