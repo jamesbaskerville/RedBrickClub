19e4e755c2641159a9feb721618e46ca8195aebf
Michael <michael@Michaels-MacBook-Pro-70.local>
2017-12-04 00:54:06 -0500

    lotta website stuff, not done

--- website/redbrickclub/redbrickclub/urls.py
+++ website/redbrickclub/redbrickclub/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^test/', include('test.urls')),
     url(r'^admin/', admin.site.urls),
+   url(r'^exchange/', include('exchange.urls'))
 ]
