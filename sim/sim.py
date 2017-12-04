ee80acf0c0ffd1dd716538335c393cf43425c611
James Baskerville <baskerville@college.harvard.edu>
2017-12-03 21:47:31 -0500

    think i added correlated preferences, sim might break

--- sim/sim.py
+++ sim/sim.py
@@ -8,18 +8,18 @@ ranks = []
 rounds = []
 
 ttc.configure_logging('warning')
-for prop in xrange(1, 11, 1):
-    res = ttc.run(1000, prop/10.0, seed = 234)
+for prop in xrange(5, 101, 5):
+    res = ttc.run(1000, prop/100.0, seed = 234)
     ranks.append((prop/10.0, res[1]))
     rounds.append((prop/10.0, res[2]))
 
-ttc_out = ranks[9][1]
+ttc_out = ranks[len(ranks) - 1][1]
 
 print "Results (pos/neg effects vs. TTC outcome)"
 print "-----------------------------------------"
 for prop, out in ranks:
     neg, pos = ttc.compare_outcomes(ttc_out, out)
-    print "{}%: (+{}, -{}), net difference: {}".format(int(prop * 100),
+    print "{}%: (+{}, -{}), net difference: {}".format(int(prop * 10),
                                                        pos,
                                                        neg,
                                                        pos - neg)
--- sim/ttc.py
+++ sim/ttc.py
@@ -1,26 +1,31 @@
 from agent import Agent
+import numpy as np
 import random
 import logging
 import sys
 
-def run(n, report_prop, seed = None, debug = False):
+def run(n, report_prop, seed = None):
     logging.info("\nBeginning Run of TTC Algorithm")
-    random.seed(seed)
+    np.random.seed(seed)
 
     agents = []
     k = int(report_prop * n)
-    available_agents = set(range(n))
+    available_agents = set(np.arange(n))
+
+    logging.info("Generating normal preference order distributions...")
+    dists = gen_pref_dists(n)
 
     logging.info("Assigning initial true and reported preferences...")
+    sample_pref_dists(n, n-2, dists)
     for i in xrange(n):
         new_agent = Agent(i)
-        true_prefs, rep_prefs = gen_prefs(n, k, debug)
-        if debug:
-            print new_agent, "; prefs:", true_prefs, rep_prefs
+        #true_prefs, rep_prefs = gen_prefs(n, k, debug)
+        true_prefs, rep_prefs = sample_pref_dists(n, k, dists)
+        logging.debug("{}; prefs: {}, {}".format(new_agent, true_prefs, rep_prefs))
         new_agent.init_preferences(available_agents, true_prefs, rep_prefs)
         agents.append(new_agent)
 
-    assigned_items = [-1] * n
+    assigned_items = np.empty(n, dtype=int)
 
     logging.info("Running TTC...")
     rounds = 0
@@ -62,28 +67,44 @@ def run(n, report_prop, seed = None, debug = False):
         # remove cycle from market
         available_agents -= set(cycle)
         logging.debug("remaining agents: {}\n".format(available_agents))
-    return assigned_items, get_pref_outcomes(agents, assigned_items), rounds
+    return assigned_items, get_pref_outcomes(agents, n), rounds
 
 # a list of the result of the algorithm for each agent
 # (e.g. their preference for the item which they were assigned)
-def get_pref_outcomes(agents, assigned_items):
-    return map(lambda (a, i): agents[a].get_top_pref_index(),
-               enumerate(assigned_items))
+def get_pref_outcomes(agents, n):
+    vfunc = np.vectorize(lambda a: agents[a].get_top_pref_index())
+    return vfunc(np.arange(n))
 
-# generate a random preference order, ranking k out of n items (k <= n)
-def gen_prefs(n, k, debug = False):
+# generate a preference order distribution for each item
+# each distribution is a normal with a random SD and mean = item number
+# we assume lower-numbered items are, on average, more valuable WLOG
+def gen_pref_dists(n):
+    means = np.arange(n, dtype = float)
+    # TODO experiement with different SD scalings? 1.5 seems to work ok
+    sdevs = np.random.random(n) * 1.5
+    logging.debug("Pref dist means and SDs:\n{}\n{}".format(means, sdevs))
+    return means, sdevs
+
+# sample over item preference order distributions to generate a pref order
+# rank in order in the floats generated--both true and reported prefs
+def sample_pref_dists(n, k, (means, sdevs)):
     try:
-        assert k <= n
+        assert k <= n and len(means) == n and len(sdevs) == n
     except AssertionError:
-        raise ValueError("Prefs should have max length of n")
-    true_prefs = range(n)
-    random.shuffle(true_prefs)
-    rep_indices = set(random.sample(range(n), k))
-    rep_prefs = map(lambda (_, p): p,
-                    filter(lambda (a, _): a in rep_indices,
-                           enumerate(true_prefs)))
-    logging.debug("{} {} {}".format(true_prefs, rep_indices, rep_prefs))
-    return true_prefs, rep_prefs
+        raise ValueError("Wrong number of reports or preference distributions")
+
+    true_prefs = np.stack((np.arange(n), np.random.normal(means, sdevs, n)), axis=-1)
+
+    # numpy makes it hard to sort by column...
+    true_prefs.view('i8,f8').sort(order=['f1'], axis=0)
+    # strip rankings, leaving just the strict preference order
+    true_prefs = np.delete(true_prefs, 1, 1)
+
+    # report preference on k items, keeping order from the true preferences
+    rep_prefs = true_prefs[np.sort(np.random.choice(np.arange(n), size=k, replace=False))]
+    logging.debug("prefs: {},\n {}".format(true_prefs, rep_prefs))
+
+    return true_prefs.flatten().astype(int), rep_prefs.flatten().astype(int)
 
 def configure_logging(loglevel):
     numeric_level = getattr(logging, loglevel.upper(), None)
@@ -103,7 +124,24 @@ def compare_outcomes(ttc, other):
 
 if __name__ == '__main__':
     configure_logging('info')
-    res = run(10, 0.75, seed = 124)
+    res = run(10, 0.75, seed = 1234)
     print "items:    {}".format(res[0])
     print "outcomes: {}".format(res[1])
     print "rounds:   {}".format(res[2])
+
+### DEPRECATED ###
+
+# generate a uniform random preference order, ranking k out of n items (k <= n)
+def gen_prefs(n, k):
+    try:
+        assert k <= n
+    except AssertionError:
+        raise ValueError("Prefs should have max length of n")
+    true_prefs = range(n)
+    np.random.shuffle(true_prefs)
+    rep_indices = set(np.random.choice(np.arange(n), size = k, replace = False))
+    rep_prefs = map(lambda (_, p): p,
+                    filter(lambda (a, _): a in rep_indices,
+                           enumerate(true_prefs)))
+    logging.debug("{} {} {}".format(true_prefs, rep_indices, rep_prefs))
+    return true_prefs, rep_prefs
